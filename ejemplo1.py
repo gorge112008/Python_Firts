29b2fbffe91f2afddb94d506e8ec1a2c803206f4
Jorge Carlos Gayoso Ugas <gorgexc4@gmail.com>
2023-07-13 02:05:40 -0500

    Add files via upload

---
+++ ejemplo1.py
@@ -0,0 +1,51 @@
+print ("""
+*****************************************
+*** DECLARACION DE VARIABLES ***
+*****************************************
+""")
+
+numero1=2
+numero2="3"
+numero3=2.75
+
+print (numero1)
+print (numero2)
+print (numero3)
+
+print (type(numero1))
+print (type(numero1))
+print (type(numero1))
+
+print ("""
+*****************************************
+*** DECLARACION DE LISTAS ***
+*****************************************
+""")
+
+lista_lenguajes= ["Pyhton", "Java", "C", "C++"]
+print(lista_lenguajes[1])
+
+print ("""
+*****************************************
+*** DECLARACION DE TUPLAS ***
+*****************************************
+""")
+
+tuplas_lenguajes=("Pyhton", "Java", "C", "C++")
+print(tuplas_lenguajes[1])
+
+print ("""
+*****************************************
+*** DECLARACION DE DICCIONARIO ***
+*****************************************
+""")
+
+diccionario_lenguajes={"cod1":"Python","cod75":"Java","cod2":"C"}
+print(diccionario_lenguajes["cod75"])
+
+
+
+
+
+
+
